dbf59da7b90b46a71e13decbeaa3e3956ba728ba
prusovak2 <prusova.k2@seznam.cz>
2020-05-10 15:27:39 +0200

    bank hw finished

--- Haskel/bank/logy.hs
+++ Haskel/bank/logy.hs
@@ -1,4 +1,5 @@
 import Data.List
+
 type Castka = Integer
 
 data Operace
@@ -63,8 +64,7 @@ demoPocetMarie = length . filter uzivatelMarie
 {- Ukol zacina tady. Misto `undefined` dodejte definice funkci, ktere z logu
  - vytahnou pozadovany vysledek. -}
 
-getVybery x = map ( foldl addOrSubtract ("", 0) ( groupBy isSameUser (sortOn getName $ filter isDepositOrWithdrawal x ) )  )
-
+getVybery x = countAverage $ sumAndLenght $ map getMoney $ filter isJUser $ filter isWithdrawal x
 -- Seznam uživatelů (bez duplicit), seřazený podle abecedy
 serazeniUzivatele :: Zaznamy -> [Uzivatel]
 serazeniUzivatele x = nub $ map getUser (sortOn getName x ) 
@@ -75,7 +75,7 @@ top10vyber x = map getTime $ take 10 (sortOn getNegativeWithdrawal (filter isWit
 
 -- Jména uživatelů, kterým přišlo deset nejmenších přípisů (bez opakování jmen)
 top10pripis :: Zaznamy -> [Uzivatel]
-top10pripis x = take 10 $ nub $ map getUser (sortOn getDeposit (filter isDeposit x) ) 
+top10pripis x = take 5 $ nub $ map getUser (sortOn getDeposit (filter isDeposit x) ) 
 
 -- Jméno uživatele, který je nejaktivnější (tj. má v logu nejvíc záznamů)
 topUzivatel :: Zaznamy -> Uzivatel
@@ -83,15 +83,15 @@ topUzivatel x = getUserFromUsrLenTuple $ head $ sortBy (\(_, a) (_, b) -> compar
 
 -- Jméno uživatele, kterému na účtu přibylo nejvíc peněz (tj. má maximální součet příjmů mínus součet výdajů)
 topPrirustek :: Zaznamy -> Uzivatel
-topPrirustek = undefined
+topPrirustek x = getUserFromUsrTotalTuple $ maximumBy (\(_, a) (_, b) -> compare a b) ( map ( foldr (addOrSubtract) ("abraka", 0) ) ( groupBy isSameUser (sortOn getName $ filter isDepositOrWithdrawal x ) ) )
 
 -- BONUS: Průměrná částka (oříznutá na celé číslo), kterou vybrali uživatelé začínající od J
 prumerVyberuJ :: Zaznamy -> Castka
-prumerVyberuJ = undefined
+prumerVyberuJ x = countAverage $ sumAndLenght $ map getMoney $ filter isJUser $ filter isWithdrawal x
 
 -- BONUS: Jméno uživatele, který provedl nejvíc akcí za sebou bez toho, aby jakýkoliv jiný uživatel cokoliv udělal (tj. po seřazení logu podle času bude mít “nejvíc řádků po sobě”)
 nejdelsiSingleRun :: Zaznamy -> Uzivatel
-nejdelsiSingleRun = undefined
+nejdelsiSingleRun x = getUserFromUsrLenTuple $ maximumBy (\(_, a) (_, b) -> compare a b) ( map getLenghtAndUser $ group $ map getName $ sortOn getTime x )
 
 getName :: Zaznam -> String
 getName (Zaznam _ name _ ) = name
@@ -99,11 +99,13 @@ getName (Zaznam _ name _ ) = name
 getUser :: Zaznam -> Uzivatel
 getUser (Zaznam _ user _ ) = user
 
-isSameUser :: Zaznam -> Zaznam -> Bool
-isSameUser (Zaznam _ user1 _ ) (Zaznam _ user2 _ ) = user1 == user2
-
 getTime :: Zaznam -> Cas
 getTime (Zaznam time _ _ ) = time
+
+getMoney :: Zaznam -> Castka
+getMoney (Zaznam _ _  (Vyber money) ) = money
+getMoney (Zaznam _ _  (Pripis money) ) = money
+getMoney (Zaznam _ _ _ ) = error "Invalid Record"
 
 getNegativeWithdrawal :: Zaznam -> Castka
 getNegativeWithdrawal (Zaznam _ _  (Vyber money) ) = money * (-1)
@@ -116,6 +118,10 @@ getWithdrawal (Zaznam _ _ _ ) =  0
 isWithdrawal :: Zaznam -> Bool
 isWithdrawal (Zaznam _ _  (Vyber _) ) = True
 isWithdrawal (Zaznam _ _ _ ) = False
+
+isSameUser :: Zaznam -> Zaznam -> Bool
+isSameUser (Zaznam _ user1 _ ) (Zaznam _ user2 _ ) = user1 == user2
+
 
 getDeposit :: Zaznam -> Castka
 getDeposit (Zaznam _ _  (Pripis money) ) = money 
@@ -130,6 +136,13 @@ isDepositOrWithdrawal (Zaznam _ _  (Pripis _) ) = True
 isDepositOrWithdrawal (Zaznam _ _  (Vyber _) ) = True 
 isDepositOrWithdrawal (Zaznam _ _ _ ) = False
 
+isJUser :: Zaznam -> Bool
+isJUser (Zaznam _ user _ ) = isJName user
+
+isJName :: Uzivatel -> Bool
+isJName ('J': _ ) = True
+isJName _ = False
+
 --takes list of multiple occurences of one user and returns User and number of its occurences
 getLenghtAndUser :: [Uzivatel] -> (Uzivatel, Int)
 getLenghtAndUser x = (head x, length x)
@@ -137,7 +150,22 @@ getLenghtAndUser x = (head x, length x)
 getUserFromUsrLenTuple :: (Uzivatel, Int) -> Uzivatel
 getUserFromUsrLenTuple (usr, _ ) = usr
 
+getUserFromUsrTotalTuple :: (Uzivatel, Integer) -> Uzivatel
+getUserFromUsrTotalTuple (usr, _ ) = usr
+
 addOrSubtract :: Zaznam -> (Uzivatel, Integer) -> (Uzivatel, Integer)
 addOrSubtract (Zaznam _ usr  (Pripis  money) ) (_, total) = (usr, ((+) total money)) 
 addOrSubtract (Zaznam _ usr  (Vyber money) ) (_, total) = (usr, ((-) total money)) 
 addOrSubtract (Zaznam _ _ _ ) (_ , _) = error "Invalid Record"
+
+sumAndLenght ::  [Integer] -> (Integer, Int)
+sumAndLenght xs = (soucet, delka)
+  where
+    soucet = foldr (+) 0 xs
+    delka = length xs 
+
+countAverage :: (Integer, Int) -> Integer
+countAverage (sm , len) = (div) sm  (toInteger len)  
+
+
+
