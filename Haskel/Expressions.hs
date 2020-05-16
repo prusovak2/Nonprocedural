082bee30e7560cc315d108a37cb39e1f6f43f6d5
prusovak2 <prusova.k2@seznam.cz>
2020-05-16 12:46:03 +0200

    probably capable of generating expressions

--- Haskel/Expressions.hs
+++ Haskel/Expressions.hs
@@ -12,7 +12,7 @@ instance Show Op where
     show Times = "*"
 
 instance Show Exp where
-    show (Const x) = show x 
+    show (Const x) = show x ++"c"
     show (Oper op arg1 arg2) = strArg1 ++" "++ show op++ " " ++ strArg2
         where
             strArg1 
@@ -26,5 +26,32 @@ isConst :: Exp -> Bool
 isConst (Const _ ) = True
 isConst _ = False
 
-split :: [Int] -> [([Int],[Int])]
+intToConst :: Int -> Exp
+intToConst n = (Const n)
+
+listIntToListConst :: [Int] -> [Exp]
+listIntToListConst = map intToConst
+
+split :: [a] -> [([a],[a])]
+split [x] = error "split: list too short"
 split xs = [(splitAt len xs )| len <- [1..((length xs)-1)] ]
+
+--tryAll :: Exp -> Exp -> (Exp,Exp,Exp)
+--tryAll ex1 ex2 = (plus, minus, mult) where
+--    plus = (Oper Plus ex1 ex2)
+--    minus = (Oper Minus ex1 ex2)
+--    mult = (Oper Times ex1 ex2)
+operators = [Plus, Minus, Times]
+
+generate:: [Exp] -> [Exp]
+generate [(Const a)] = [(Const a)]
+generate xs = [ (Oper op arg1 arg2) | (list1, list2) <- split xs , op <- operators, arg1 <- (generate list1), arg2 <- (generate list2)  ]
+
+generateArit :: [Int] -> [Exp]
+generateArit xs = generate $ listIntToListConst xs
+
+
+--generate xs = [ [(Oper op arg1 arg2) |  op <- operators, arg1 <- (generate list1), arg2 <-(generate list2) ] | (list1, list2) <- split xs ]
+--generate :: [Int] -> [Exp]
+--generate [x] = (Const x)
+--generate xs = split xs
